7e5c1840542356fffbd24cb14a62aef84cbc22b5
Abdoul Khadr Sylla <abdoulkhadrsylla221@gmail.com>
2026-06-30 22:29:59 +0000

    last espoir

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,14 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
+import viteReact from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({});
+export default defineConfig({
+  plugins: [
+    tanstackStart(),
+    nitro({ preset: "vercel" }),
+    viteReact(),
+    tsconfigPaths(),
+  ],
+});
